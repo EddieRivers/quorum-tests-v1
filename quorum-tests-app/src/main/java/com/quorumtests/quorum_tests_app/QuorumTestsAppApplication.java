d4ab883874815b2f35c6cc5559db942581fc9fdd
EddieRivers <eduardogallego2020@gmail.com>
2025-03-28 14:10:57 +0100

    Initial app configuration done

---
+++ quorum-tests-app/src/main/java/com/quorumtests/quorum_tests_app/QuorumTestsAppApplication.java
@@ -0,0 +1,13 @@
+package com.quorumtests.quorum_tests_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuorumTestsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuorumTestsAppApplication.class, args);
+	}
+
+}
